ec426061e4e8be284d617f6d9032a14dfc97d5a7
Beongu Cho <whqjarn111@gcdc.co.kr>
2025-07-06 05:38:32 +0900

    CORS allow header 정보 추가

--- src/config/cors_config.rs
+++ src/config/cors_config.rs
@@ -1,14 +1,17 @@
 pub mod cors_setting {
-    use axum::http::{Method};
+    use axum::http::{
+        Method
+    };
+    use http::header::{CONTENT_TYPE};
     use tower_http::cors::{Any, CorsLayer};
+    
 
     pub fn cors_setting() -> CorsLayer {
         let cors: CorsLayer = CorsLayer::new()
             .allow_methods([Method::GET, Method::POST])
-            .allow_origin(Any);
+            .allow_origin(Any)
+            .allow_headers([CONTENT_TYPE]);
 
         cors
     }
-    
-
 }
